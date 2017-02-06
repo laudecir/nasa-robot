afc9c843a53f3b94d4ec35f457af0130c69d91b0
Laudecir Hasckel <laudecir.hasckel@thomsonreuters.com>
2017-02-06 14:45:44 -0200

    Trocado o tipo de retorno do serviço REST de String para Response

--- src/main/java/gov/nasa/robot/rest/MarsREST.java
+++ src/main/java/gov/nasa/robot/rest/MarsREST.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Path("/mars")
 public class MarsREST {
@@ -17,13 +18,13 @@ public class MarsREST {
 
     @POST
     @Path("/{steps}")
-    public String controlRobot(@PathParam("steps") String steps) {
+    public Response controlRobot(@PathParam("steps") String steps) {
         try {
             robot.moveBySteps(steps);
-            return robot.getPosition();
+            return Response.ok(robot.getPosition()).build();
         } catch (OutsideAreaException ex) {
             Logger.getLogger(MarsREST.class.getName()).log(Level.SEVERE, "BAD REQUEST", ex);
-            return "400 Bad Request";
+            return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
 }
